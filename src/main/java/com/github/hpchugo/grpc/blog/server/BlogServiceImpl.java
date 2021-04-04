2f40c4e6b18cd3e6f2db0f0e1517c7b27a40d197
Hugo Cordeiro <hp.cordeiro@hotmail.com>
2021-04-04 19:12:14 -0300

    Create blog implementation

--- src/main/java/com/github/hpchugo/grpc/blog/server/BlogServiceImpl.java
+++ src/main/java/com/github/hpchugo/grpc/blog/server/BlogServiceImpl.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.proto.blog.Blog;
 import com.proto.blog.BlogServiceGrpc;
 import com.proto.blog.CreateBlogRequest;
 import com.proto.blog.CreateBlogResponse;
@@ -15,9 +16,25 @@ public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
 
     private MongoClient mongoClient = MongoClients.create("mongodb:localhost:27017");
     private MongoDatabase database = mongoClient.getDatabase("mydb");
-    private MongoCollection<Document> collection = database.getCollection("blog");
+    private MongoCollection<Document> collection = database.getCollection("blog");//table
     @Override
     public void createBlog(CreateBlogRequest request, StreamObserver<CreateBlogResponse> responseObserver) {
-        super.createBlog(request, responseObserver);
+        System.out.println("Received Create Blog request");
+        Blog blog = request.getBlog();
+        Document doc = new Document("author_id", blog.getAuthorId())
+                .append("title", blog.getTitle())
+                .append("content", blog.getContent());
+        //insert (create) the document in MongoDB
+        System.out.println("Inserting blog...");
+        collection.insertOne(doc);
+
+        //retrieve the MongoDB generated ID
+        String id = doc.getObjectId("_id").toString();
+        System.out.printf("Inserted blog: %s\n", id);
+
+        CreateBlogResponse response = CreateBlogResponse.newBuilder()
+                .setBlog(blog.toBuilder().setId(id).build()).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
